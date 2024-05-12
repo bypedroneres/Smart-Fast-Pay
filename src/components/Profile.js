dce07cd431a0ca89946f81419f6ce3e346aa66b3
Pedro Neres <pedroneres@Pedros-MacBook-Air.local>
2024-05-12 15:20:30 -0300

    changed svg icons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -16,22 +16,22 @@ function Profile() {
             <div className='profile_Buttons'>
                 <div className='profile_Button'>
                     <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 6V18M12 6L7 11M12 6L17 11" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 6V18M12 6L7 11M12 6L17 11" stroke="var(--primary-white)" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </div>
                 <div className='profile_Button'>
                     <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 6V18M12 18L7 13M12 18L17 13" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 6V18M12 18L7 13M12 18L17 13" stroke="var(--primary-white)" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </div>
                 <div className='profile_Button'>
                     <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 6V18M12 6L7 11M12 6L17 11" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 6V18M12 6L7 11M12 6L17 11" stroke="var(--primary-white)" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </div>
                 <div className='profile_Button'>
                     <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 6V18M12 18L7 13M12 18L17 13" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 6V18M12 18L7 13M12 18L17 13" stroke="var(--primary-white)" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </div>
             </div>
